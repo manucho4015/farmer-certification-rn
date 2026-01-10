43a379603d66bf81dc40815378d536344c151457
Emmanuel Ochieng <eochieng16@gmail.com>
2026-01-10 20:58:47 +0300

    role selection screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import type { UserRole } from '@/features/auth/authSlice'
+import { setRole } from '@/features/auth/authSlice'
+import { useRouter } from 'expo-router'
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { useDispatch } from 'react-redux'
 
-export default function Index() {
+const RoleButton = ({ label, handlePress, role }: { label: string, handlePress: (role: UserRole) => void, role: UserRole }) => (
+  <TouchableOpacity onPress={() => handlePress(role)} className='bg-white p-6 rounded-xl shadow mb-4'>
+    <Text className='font-bold capitalize'>{label}</Text>
+  </TouchableOpacity>
+)
+
+const RoleSelectionScreen = () => {
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const handleSelectRole = (roleId: UserRole) => {
+    dispatch(setRole(roleId))
+
+    roleId === 'farmer' && router.navigate('/(root)/farmer/registration')
+  }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1 bg-gray-50 px-6 py-12 justify-center'>
+      <Text className='text-3xl font-bold mb-8 text-center'>Select Your Role</Text>
+
+      <RoleButton label='farmer' handlePress={handleSelectRole} role='farmer' />
+      <RoleButton label='admin' handlePress={handleSelectRole} role='admin' />
     </View>
-  );
+  )
 }
+
+export default RoleSelectionScreen
