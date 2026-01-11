d6cd7001b56eb34ff191e0962ed2d94b040092f5
Emmanuel Ochieng <eochieng16@gmail.com>
2026-01-11 21:56:06 +0300

    implement login screen

---
+++ app/(root)/farmer/login.tsx
@@ -0,0 +1,50 @@
+import type { RootState } from "@/app/store";
+import arrowLeft from '@/assets/images/material-symbols--arrow-left.png';
+import { setFarmerId } from '@/features/auth/authSlice';
+import { Link, useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDispatch, useSelector } from "react-redux";
+
+const login = () => {
+    const router = useRouter()
+    const dispatch = useDispatch()
+    const farmers = useSelector((store: RootState) => store.farmers.farmers)
+
+    const [name, setName] = useState('')
+
+    const handleLogin = (name: string) => {
+        if (farmers.some(f => f.name.toLowerCase() === name.toLowerCase().trim())) {
+            const farmer = farmers.find(f => f.name.toLowerCase() === name.toLowerCase().trim())
+            if (farmer) {
+                dispatch(setFarmerId(farmer?.id))
+            }
+            router.replace('/farmer/dashboard')
+        } else {
+            alert('Farmer not found')
+        }
+    }
+    return (
+        <SafeAreaView className='h-screen relative px-8 flex flex-row items-center bg-white'>
+            <TouchableOpacity onPress={() => router.navigate('/')} className='absolute top-[7.5vh] left-8 flex flex-row gap-3 items-center mt-[10px]'>
+                <Image source={arrowLeft} className='size-8' resizeMode='contain' />
+                <Text>Home</Text>
+            </TouchableOpacity>
+
+            <View className='w-full'>
+                <TextInput value={name} onChangeText={setName} className='w-full rounded-full border-[1px] px-5' placeholder='Registered Farmer Name' />
+
+
+                <TouchableOpacity onPress={() => handleLogin(name)} className=' bg-black rounded-full shadow  mt-[25px] flex flex-row justify-center py-3'>
+                    <Text className='font-semibold text-white text-[16px]'>Check Status</Text>
+                </TouchableOpacity>
+
+                <Text className='mt-[15px] pl-4'>Don't have an account? <Link href='/(root)/farmer/registration' className='text-blue-500'>Register</Link></Text>
+            </View>
+
+        </SafeAreaView>
+    )
+}
+
+export default login
